419ba7d5501c8830f4f5b955487abb62b094de59
xiaohei21 <70131667+xiaohei21@users.noreply.github.com>
2023-08-16 16:30:08 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,18 +23,48 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'opEaE6uv6Lq_fOlFLqIrxlLK05s0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'AIeNh001-W7hBoAqe7y0Z68E3YBhYBwv8SsDHIl4kfM',
+      useTemplateId: 'MfFPh24ngOa2BHDEIMasJbxCJx1n5wIO2AwR-_XIV4E',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-02',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1998', date: '08-02',
+          type: '*生日', name: '浩', year: '1998', date: '08-02',
         },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        // // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        // {
+        //   type: '生日', name: '李四', year: '1996', date: '09-31',
+        // },
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '节日', name: '相识纪念日', year: '2022', date: '03-14',
         },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2022-11-11' },
+        // 结婚纪念日
+        //{ keyword: 'marry_day', date: '2022-09-09' },
+      ],
+    },
+     {
+      // 想要发送的人的名字
+      name: '婵',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'opEaE6hfJsw4hVJ16VdBws09t7xE',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'LO-0Qi1C1zrBTebz1etL4NvHsyMPk6qT-eefOWdYTCU',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '10-30',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '婵', year: '1997', date: '10-30',
+        },
+        // // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        // {
+        //   type: '生日', name: '李四', year: '1996', date: '09-31',
+        // },
         {
           type: '节日', name: '相识纪念日', year: '2022', date: '03-14',
         },
@@ -48,7 +78,6 @@ const USER_CONFIG = {
       ],
     },
   ],
-
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
   CALLBACK_TEMPLATE_ID: 'uhuM3gRJcGWqIrjsuAfEHsLxIT8nMa1wPGQvWVFdEU4',
